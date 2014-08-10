5b16f8aae418da282e59c79bbddff8bef4ce8abe
Shannon Tsaltas <shannontsaltas@gmail.com>
2014-08-10 16:45:56 -0500

    implemented pagination feature

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,48 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+import mistune
 
 from blog import app
 from database import session
 from models import Post
 
 @app.route("/")
-def posts():
+@app.route("/page/<int:page>")
+def posts(page=1, paginate_by=10):
+    # Zero-indexed page
+    page_index = page - 1
+
+    count = session.query(Post).count()
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) / paginate_by + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
     posts = session.query(Post)
     posts = posts.order_by(Post.datetime.desc())
-    posts = posts.all()
+    posts = posts[start:end]
+
     return render_template("posts.html",
         posts=posts,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
     )
+
+@app.route("/post/add", methods=["GET"])
+def add_post_get():
+	return render_template("add_post.html")
+
+@app.route("/post/add", methods=["POST"])
+def add_post_post():
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
