8cb722d1e413bfa5014a64fd7f36090faeed3c57
Shannon Tsaltas <shannontsaltas@gmail.com>
2014-12-20 17:02:37 -0600

    started testing blog application

--- blog/config.py
+++ blog/config.py
@@ -4,3 +4,8 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///blog-development.db"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
+
+class TestingConfig(object):
+	SQLALCHEMY_DATABASE_URI = "sqlite:///blog-testing.db"
+	DEBUG = False
+	SECRET_KEY = "Not secret"
